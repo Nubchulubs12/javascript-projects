57cf1529d1270b19d09c50c11e3f37a210b1498a
Nubchulubs12 <nick.kuczka@gmail.com>
2024-06-12 17:51:41 -0500

    moon

--- data-and-variables/exercises/data-and-variables-exercises.js
+++ data-and-variables/exercises/data-and-variables-exercises.js
@@ -9,3 +9,35 @@
 // Calculate a trip to the moon below
 
 // Print the results of the trip to the moon below
+let spaceShuttle = "Determination";
+let shuttleSpeedMph = 17500;
+let distanceToMars = 225000000;
+let distanceToTheMoon = 384400;
+const MilesPerKilometer = 0.621;
+console.log(typeof(spaceShuttle));
+console.log(typeof(shuttleSpeed));
+console.log(typeof(distanceToMars));
+console.log(typeof(distanceToTheMoon));
+console.log(typeof(MilesPerKilometer));
+
+let milesToMars = distanceToMars * MilesPerKilometer;
+console.log(milesToMars);
+
+let hoursToMars = milesToMars / shuttleSpeedMph;
+console.log(hoursToMars);
+
+let daysToMars = hoursToMars / 24;
+console.log(daysToMars);
+
+console.log((spaceShuttle) + " will take " + (daysToMars) + "days to reach Mars.");
+
+let milesToTheMoon = distanceToTheMoon * MilesPerKilometer;
+console.log(milesToTheMoon);
+
+let hoursToMoon = milesToTheMoon / shuttleSpeedMph;
+console.log(hoursToMoon);
+
+let dayToMoon = hoursToMoon /24;
+console.log(dayToMoon);
+
+console.log((spaceShuttle) + " will take " + (dayToMoon) + "days to reach the Moon.");
