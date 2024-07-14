44c609d06d8c6b68786676c66143eb03bfea58d7
Nubchulubs12 <nick.kuczka@gmail.com>
2024-07-14 15:18:43 -0500

    rps done

--- unit-testing/exercises/RPS.js
+++ unit-testing/exercises/RPS.js
@@ -15,6 +15,14 @@ function whoWon(player1,player2){
    if (player1 === 'scissors' && player2 === 'rock '){
      return 'Player 2 wins!';
    }
- 
+   if (!isValidOption(player1) || !isValidOption(player2)) {
+    return "thats not an option.";
+   }
    return 'Player 1 wins!';
  }
+ function isValidOption(option) {
+  return (option === 'rock' || option === 'paper' || option === 'scissors');
+}
+ 
+
+ module.exports = whoWon;
