8dc303bb8bc80e32db806b14718da67a3c7bea69
Nubchulubs12 <nick.kuczka@gmail.com>
2024-08-11 11:10:26 -0500

    planets

---
+++ fetch/exercises/json.js
@@ -0,0 +1,22 @@
+{
+    "type": "dog",
+        "name": "Bernie",
+            "age": 3
+}
+// this is the correct setup for json because it has to have double quotes for keys and properties.
+
+{
+    "animals": [
+        {
+            "type": "dog",
+            "name": "Bernie",
+            "age": 3
+        },
+        {
+            "type": "cat",
+            "name": "Draco",
+            "age": 2
+        }
+    ]
+}
+// this is the correct setup for json because the object name is in double quotes and so are the key and properties.
