44d76ece6ccc8c04a6b59faed0728267e8b615fd
Nubchulubs12 <nick.kuczka@gmail.com>
2024-08-22 14:35:12 -0500

    done

--- react-exercises-and-studio/part2/studio/src/components/Ingredients.jsx
+++ react-exercises-and-studio/part2/studio/src/components/Ingredients.jsx
@@ -1,9 +1,21 @@
+import recipedata from "./recipe.json";
+
 function IngredientList() {
-   return;
- }
- 
- export default IngredientList;
- 
+let ingredient = recipedata[0].ingredients.map((ingredient,index) => {
+  return <li key={index}>{ingredient}</li>
+});
+
+  return (
+    <div>
+      <h3>Ingredients</h3>
+      <ul>
+      {ingredient}
+      </ul>
+    </div>
+  );
+}
+
+export default IngredientList;
+
 //import json file for the data
 //use a nested map to get inside the inner array
- 
--- react-exercises-and-studio/part2/studio/src/components/RecipeImage.jsx
+++ react-exercises-and-studio/part2/studio/src/components/RecipeImage.jsx
@@ -1,9 +1,14 @@
+import recipedata from "./recipe.json";
+import "./styling.css";
 function RecipeImage() {
-   return;
- }
- 
- export default RecipeImage;
- 
- //import json file for the data
- //apply css for className recipeImage
- 
+  return(
+    <div>
+      <img className="recipeImage" src={recipedata[0].recipeImage} alt={recipedata[0].name}></img>
+    </div>
+  );
+}
+
+export default RecipeImage;
+
+//import json file for the data
+//apply css for className recipeImage
